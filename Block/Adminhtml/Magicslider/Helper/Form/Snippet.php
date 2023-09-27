ee077f94f5e6c8d6c01696ad1e6f5d4c736f2c34
Alex Dong <team.magiccart@gmail.com>
2023-09-27 23:57:52 +0700

    Improve

--- Block/Adminhtml/Magicslider/Helper/Form/Snippet.php
+++ Block/Adminhtml/Magicslider/Helper/Form/Snippet.php
@@ -14,18 +14,17 @@ class Snippet extends AbstractElement
         $slider   = $this->getSlider();
         $identifier = $slider->getIdentifier() ?? '$identifier';
         $shortcodeWidget = '{{widget type="Magiccart\Magicslider\Block\Widget\Slider" identifier="' . $identifier . '" template="magicslider.phtml"}}';
-        $shortcodeBlock  = $this->_escaper->escapeHtml('<?= $block->getLayout()->createBlock(\'Magiccart\Magicslider\Block\Widget\Slider\')->setIdentifier("' . $identifier . '")->toHtml();?>');
-        $html = '<ul class="magicslider-snippet" style="list-style: none;"><li>';
+        $shortcodeBlock  = $this->_escaper->escapeHtml('<?= $block->getLayout()->createBlock(\'Magiccart\Magicslider\Block\Widget\Slider\')->setIdentifier("' . $identifier . '")->setTemplate(\'magicslider.phtml\')->toHtml(); ?>');
+        $html = '<ol class="magicslider-snippet"><li>';
         $html .= '<span>' . __('Add Widget name "Magiccart Magicslider widget" and set Identifier or MagicsliderId for it.') . '</span>';
         $html .= '</li><li>';
         $html .= '<span>' . __('CMS Page/Static Block') . '</span>';
         $html .= '<code>' . $shortcodeWidget . '</code>';
         $html .= '<p>' . __('You can paste the above block of snippet into any page in Magento 2 and set Identifier or MagicsliderId for it.') . '</p>';
         $html .= '</li><li>';
-        $html .= '<span>' . __('Template .phtml file') . '</span>';
+        $html .= '<span>' . __('Template .phtml file. Open a .phtml file and insert where you want to display Banner Slider.') . '</span>';
         $html .= '<code>' . $shortcodeBlock . '</code>';
-        $html .= '<p>' . __('Open a .phtml file and insert where you want to display Banner Slider.') . '</p>';
-        $html .= '</li></ul>';
+        $html .= '</li></ol>';
 
         return $html;
     }
